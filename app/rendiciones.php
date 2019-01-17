2b4423c45cedfba7e1db088636983fdd519a4228
Esteban <e.pino04@ufromail.cl>
2019-01-17 00:12:21 -0300

    Initial commit

---
+++ app/rendiciones.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class rendiciones extends Model
+{
+    //
+
+    protected $fillable = ['fondo', 'fechaInicio', 'fechaFin','total','estado'];
+}
+
+
